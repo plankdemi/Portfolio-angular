d7b3f7994ffd1c535936490ef27717aa7200bff0
Vladas <baradulinasvladas@gmail.com>
2023-05-26 13:31:35 +0300

    installed flowbite

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header-home/header.component';
@@ -9,6 +8,7 @@ import { MainComponent } from './main/main.component';
 import { Routes, RouterModule } from '@angular/router';
 import { ProjectsComponent } from './projects/projects.component';
 import { HeaderProjectsComponent } from './header-projects/header-projects.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 const appRoutes: Routes = [
   { path: '', component: MainComponent },
@@ -23,6 +23,7 @@ const appRoutes: Routes = [
     MainComponent,
     ProjectsComponent,
     HeaderProjectsComponent,
+    CarouselComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
   providers: [],
